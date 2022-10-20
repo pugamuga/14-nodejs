07812c0e5e91fc760b472eb29a059d6465d9a9d7
Evgeny Pugachev <rabota.pugamuga@gmail.com>
2022-10-20 18:59:52 +0300

    learn threads

--- smallDemo/lord_of_rings/time.js
+++ smallDemo/lord_of_rings/time.js
@@ -1,9 +1,40 @@
-const sec = 100;
-const start= performance.now()
+// const sec = 10000;
+// const start = performance.now();
 
+// const ten = setTimeout(() => {
+//   console.log(sec / 1000 + " sec passed");
+// }, sec);
 
-setTimeout((data = "nothing") => {
-    console.log(((performance.now() - start)/1000).toFixed(3) + " sec")
-  console.log(sec / 1000 + " sec passed");
-  console.log("and here my data: " + data)
-}, sec);
+// setTimeout(() => {
+//   clearTimeout(ten);
+//   console.log("ten killed");
+//   console.log(performance.now()/1000 + " sec")
+// }, sec / 10);
+
+const fs = require("fs");
+
+console.log("start");
+
+setTimeout(() => {
+  console.log(performance.now(), "timer 0");
+}, 0);
+
+setImmediate(() => {
+  console.log("Immediate");
+});
+
+fs.readFile("../../text.txt", () => {
+  console.log("file is readed:", performance.now());
+});
+
+Promise.resolve().then(() => {
+  console.log("Promise here");
+});
+const bigFunc = (hardLevel) => {
+  for (let i = 0; i < hardLevel; i++) {}
+  console.log("done!!!");
+};
+
+bigFunc(1000000000);
+
+console.log("finish");
