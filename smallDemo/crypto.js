4e3a6aa3ccc48d74cdfd73779a714efc5475c402
Evgeny Pugachev <rabota.pugamuga@gmail.com>
2022-10-21 11:08:06 +0300

    windows sucks

--- smallDemo/crypto.js
+++ smallDemo/crypto.js
@@ -1,3 +1,18 @@
 const crypto = require("crypto");
+const OS = require("os")
 
-console.log(crypto)
+console.log(OS.cpus().length)
+
+process.env.UV_THREADPOOL_SIZE = OS.cpus().length
+
+console.log(process.env.UV_THREADPOOL_SIZE)
+
+const start = performance.now();
+
+// console.log(process.env.UV_THREADPOOL_SIZE)
+
+for (let i = 0; i < 16; i++) {
+  crypto.pbkdf2("test", "salt", 1000000, 64, "sha256", () => {
+    console.log(performance.now() - start);
+  });
+}
